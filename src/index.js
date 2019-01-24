8e8493ef3f3d4f68823ad88a1ccad36c4c35b5f0
Seolhee Kim <seolheekim808@gmail.com>
2019-01-23 17:08:24 -0800

    deleted all react app files and created new files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//this is a class component
+class App extends React.Component {
+  state = {
+    latitude: null,
+    errorMessage: ''
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ latitude: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    )
+  };
+
+  //react says you HAVE to define render
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div> Error: {this.state.errorMessage} </div>
+    }
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <SeasonDisplay latitude={this.state.latitude} />
+    }
+    return <Spinner message="Please accept location request" />;
+  };
+};
+
+ReactDom.render(<App />, document.querySelector('#root'));
+
+  // constructor() is a specially named function that is particular to
+  // the Javascript language not specific to react.
+  // In Javascript class the constuctor() function is the very first
+  // function that is going to be called any time an instance of this
+  // class is created. So any time a it creates a new instance of the
+  // app component and shows it on the screen the constructor() function // is going to automatically call it before anything else.
+
+  // constructor(props) {
+
+  // super is a reference to the parents constuctor fucntion.
+  // have to call super(props) evertime.
+
+  // super(props);
+
+  // this is a state object, it contains different pieces of data &
+  // some different properties that are very important and relevant
+  // to the component that it is being put together.
+  // THIS IS THE ONLY TIME we do direct assignment to this.state.
+
+  //   this.state = {
+  //     latitude: null,
+  //     errorMessage: ''
+  //   };
+
+  //  'Component Lifecycle Methods' is a function that we can optionally
+  //  define inside of our class based components if we decide to
+  //  implement these menthods. They will be called automatically by
+  //  react at certain point during a component life cycle.
+  // };
